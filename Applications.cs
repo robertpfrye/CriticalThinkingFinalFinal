f5e1b32e908f75ad1558e1d8ba025dc64f2bf445
robertpfrye <robert.p.frye@gmail.com>
2020-05-03 14:41:34 -0700

    Created Applications Class, variables and constructor.

---
+++ Applications.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CriticalThinkingWorksheet
+{
+    class Applications
+    {
+        //member variables
+        public string ApplicationName;
+        public string ApplicationType;
+        public double RequiredRam;
+        public double RequiredStorage;
+
+        //construtors
+        public Applications()
+        {
+            ApplicationName = "Slack";
+            ApplicationType = "Messaging Service";
+            RequiredRam = 4.0; // In Gigabytes
+            RequiredStorage = 0.512; // In Gigabytes
+
+        }
+        //member methods
+
+
+
+
+
+    }
+}
